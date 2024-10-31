68cf7755cb313b9add72210dd8d8e95b3a6071a6
Anael MOBILIA <contrib@anael.eu>
2024-10-31 23:19:53 +0100

    Update bootstrap + utilisation du CDN directement

--- index.php
+++ index.php
@@ -47,8 +47,7 @@ if (isset($_FILES[FIELD_UPLOAD])) {
     <link rel="icon" type="image/png" href="<?= DEFAULT_THUMBS ?>" sizes="16x16">
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap-5.2.1.min.css" rel="stylesheet"
-          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Sticky navbar -->
     <style>
         /* Show it is fixed to the top */
@@ -150,9 +149,7 @@ if (isset($_FILES[FIELD_UPLOAD])) {
         </div>
 </main>
 
-<script src="js/bootstrap-5.2.1.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 <script>
     /**
      * Filtre les éléments affichés en fonction de la saisie de l'utilisateur
